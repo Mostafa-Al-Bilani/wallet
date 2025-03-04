3866eb2e480d3e8703e444e005c3d1e042223d39
Mostafa-Al-Bilani <mostafaalbilani@gmail.com>
2025-03-04 23:09:17 +0200

    created withdraw js

---
+++ wallet-client/scripts/withdraw.js
@@ -0,0 +1,45 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const token = localStorage.getItem("token");
+    const withdrawForm = document.getElementById("withdrawForm");
+  
+    withdrawForm.addEventListener("submit", async function (event) {
+      event.preventDefault();
+  
+      // Get input values
+      const amount = document.getElementById("withdraw-amount").value;
+  
+      try {
+        // Send request with Axios
+        const response = await axios.post(
+          "http://localhost/wallet/wallet-server/user/v1/withdraw.php",
+          { amount },
+          {
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+  
+        if (response.data.status === "success") {
+          const now = new Date();
+          const formattedTime = now.toISOString().slice(0, 19).replace("T", " ");
+          let html = `<tr>
+          <td>${amount}</td>
+          <td>withdraw</td>
+          <td>${formattedTime}</td>
+          <td>${formattedTime}</td>
+          </tr>`;
+          document.getElementById("transactions-body").innerHTML += html;
+          document.getElementById("modal-withdraw").classList.remove("active");
+        } else {
+          alert(response.data.message);
+        }
+      } catch (error) {
+        console.error(error);
+        alert(
+          "Error: " + (error.response?.data?.message || "Something went wrong")
+        );
+      }
+    });
+  });
